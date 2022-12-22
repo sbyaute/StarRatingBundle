b75d037f5b4bdd5a06b7f6d2e2e52f7c0f1a5663
Stéphane Bollard <74253670+sbyaute@users.noreply.github.com>
2022-12-22 11:45:12 +0100

    Create StarRatingExtension.php

---
+++ Extensions/StarRatingExtension.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Sbyaute\StarRatingBundle\Extensions;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class StarRatingExtension extends \Twig_Extension
+{
+
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('rating', array($this, 'rating'), array('is_safe' => array('all'))),
+        );
+    }
+
+    public function rating($number, $max = 5, $starSize = "", $inline = false)
+    {
+        $tag = 'div';
+        if ($inline) {
+            $tag = 'span';
+        }
+        return $this->container->get('twig')->render(
+            '@SbyauteStarRatingBundle/Display/ratingDisplay.html.twig',
+            array(
+                'stars' => $number,
+                'max' => $max,
+                'starSize' => $starSize,
+                'tag' => $tag,
+            )
+        );
+    }
+
+    public function getName()
+    {
+        return 'star_rating_extension';
+    }
+}
